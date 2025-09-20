4ae49ba595279238354cb3f1d6a6238b633caeda
ruohanhuang971 <ruohanhuang971@gmail.com>
2025-09-20 16:07:57 -0700

    - add command to concurrently starting both server and client - set up tailwindcss

--- Code with Mosh/chat_bot_app/index.ts
+++ Code with Mosh/chat_bot_app/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently'; // start multiple application with a single command
+
+// give an array of commands, each command for starting one application
+concurrently([
+    {
+        name: 'server',
+        command: 'bun run dev',
+        cwd: 'packages/server', // current working directory
+        prefixColor: 'cyan' // differentiate between server and client messages by color
+    },
+    {
+        name: 'client',
+        command: 'bun run dev',
+        cwd: 'packages/client',
+        prefixColor: 'green'
+    }
+]);
+
+// to run both commands -> run index.ts from root directory
